fbe79d0dea0cf1527383ee4b31ee33cfec512758
R.Kaufmann <info@kaufmann-automotive.ch>
2019-04-06 12:49:10 +0200

    Updated wrapper description.

--- rks-usb-csharp/Properties/AssemblyInfo.cs
+++ rks-usb-csharp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("rks-usb-csharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# wrapper for Kaufmann Automotive GmbH CAN-Interface DLL")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("https://www.kaufmann-automotive.ch")]
 [assembly: AssemblyProduct("rks-usb-csharp")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2019 Kaufmann Automotive GmbH")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
